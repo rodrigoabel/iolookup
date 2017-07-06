8cc42376faa1409219e04fbc04b9906a1d1e26a7
Rodrigo Abel <rodrigo.abel@gmail.com>
2017-07-06 01:40:11 +0000

    Sensont temperatura y humedad DHT11 en python

---
+++ install-lookup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Creado para LookUP en Julio de 2017, para utilizarse en 2017-06-21-raspbian-jessie-lite
+#
+echo "LookUp -- Instalación RPI3"
+#sudo apt-get update
+#sudo apt-get upgrade
+#sudo apt-get install dnsmasq hostapd
+#sudo nano /etc/dhcpcd.conf
+echo "Fin instalación"
